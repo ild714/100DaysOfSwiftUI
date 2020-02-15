12a63386fac73e6662b27fead56c7cc37950dd8c
ildar714 <ildarnigmetzjanov@gmail.com>
2020-02-15 16:51:54 +0300

    Day18

--- Project1/Project1/ContentView.swift
+++ Project1/Project1/ContentView.swift
@@ -10,15 +10,16 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var checkAmount = ""
-    @State private var numberOfPeople = 2
+    @State private var numberOfPeople = ""
     @State private var tipPercentage = 2
+    
     
     let tipPercentages = [10,15,20,25,0]
     
    
     var totalPerPerson: Double {
         let tipSelection = Double(tipPercentages[tipPercentage])
-        let peopleCount = Double(numberOfPeople + 2)
+        let peopleCount = Double(numberOfPeople) ?? 0
         let orderAmount = Double(checkAmount) ?? 0
         
         let tipValue = orderAmount / 100 * tipSelection
@@ -28,6 +29,16 @@ struct ContentView: View {
         return amountPerPerson
     }
    
+    var total: Double {
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        
+        return grandTotal
+        
+    }
     
     var body: some View {
         NavigationView{
@@ -36,24 +47,29 @@ struct ContentView: View {
                     TextField("Amount",text: $checkAmount)
                         .keyboardType(.decimalPad)
                     
-                    Picker("Number of people",selection: $numberOfPeople){
-                        ForEach(2..<100){
-                            Text("\($0) people")
-                        }
-                    }
+//                    Picker("Number of people",selection: $numberOfPeople){
+//                        ForEach(2..<100){
+//                            Text("\($0) people")
+//                        }
+//                    }
+                    TextField("Number of people",text: $numberOfPeople)
                 }
                 
                 Section(header:Text("How much tip do you want to leave?")){
                    
                     Picker("Tip percentage",selection: $tipPercentage){
                         ForEach(0..<tipPercentages.count){
-                            Text("\(self.tipPercentages[$0])")
+                            Text("\(self.tipPercentages[$0])%")
                         }
                     }.pickerStyle(SegmentedPickerStyle())
                 
                 }
                 
-                Section{
+                Section(header:Text("All amount")){
+                    Text("$\(total,specifier: "%.2f")")
+                }
+                
+                Section(header:Text("Amount per person")){
                     Text("$\(totalPerPerson,specifier: "%.2f")")
                 }
             }
